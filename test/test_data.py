11c06cd85433342f397f13e6fd3192964ae85402
A01688411 <140132383+A01688411@users.noreply.github.com>
2023-08-14 21:49:21 -0700

    Update test_data.py

--- test/test_data.py
+++ test/test_data.py
@@ -23,8 +23,8 @@ def test_csv_file_existence():
     Test case to check if the CSV file exists.
     """
     # Provide the path to your CSV file that needs to be tested
-    os.chdir('C:/Users/fzavala/Documents/GitHub/project-MLOPS')
-    csv_file_path = "./data/retrieved_data.csv"
+    #os.chdir('C:/Users/fzavala/Documents/GitHub/project-MLOPS')
+    csv_file_path = "./data/weatherAUS.csv"
 
     # Call the function to check if the CSV file exists
     file_exists = does_csv_file_exist(csv_file_path)
